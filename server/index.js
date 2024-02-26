e9ca75cf83271db7c07bda9ab0418f20868358cd
himanshu kesarwani <newarchitecture2000@gmail.com>
2024-02-26 01:31:37 -0800

    style own logo

--- server/index.js
+++ server/index.js
@@ -1,9 +1,17 @@
 require("dotenv").config();
 const express = require("express");
 const { connectToMongoDB } = require("./database");
+const path = require("path");
 
 const app = express();
 app.use(express.json());
+
+// deployement
+app.use(express.static(path.join(__dirname, "build")))
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "build/index.html"))
+})
+
 
 const router = require("./routes");
 app.use("/api", router);
