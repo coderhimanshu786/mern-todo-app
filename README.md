dfe4f73c2383f6b1d49b32d3d9e870e018619ba2
himanshu kesarwani <newarchitecture2000@gmail.com>
2024-02-26 03:35:34 -0800

    styling background

---
+++ README.md
@@ -0,0 +1,3 @@
+app thumbnail -
+---
+![Screenshot (96)](https://github.com/coderhimanshu786/mern-todo-app/assets/113245834/32bbdabb-7e44-4fa3-8bfd-d336daccb69f)
--- client/src/todo.jsx
+++ client/src/todo.jsx
@@ -44,8 +44,8 @@ export default function Todo(props) {
           onClick={() => updateTodo(todo._id, todo.status)}
         >
           {todo.status ? 
-          <svg xmlns="http://www.w3.org/2000/svg" height="30" viewBox="0 -960 960 960" width="30"><path d="m424-312 282-282-56-56-226 226-114-114-56 56 170 170ZM200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h560q33 0 56.5 23.5T840-760v560q0 33-23.5 56.5T760-120H200Zm0-80h560v-560H200v560Zm0-560v560-560Z"/></svg> :
-          <svg xmlns="http://www.w3.org/2000/svg" height="28" viewBox="0 -960 960 960" width="28"><path d="M200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h560q33 0 56.5 23.5T840-760v560q0 33-23.5 56.5T760-120H200Zm0-80h560v-560H200v560Z"/></svg>
+          <div className="svg-1"><svg xmlns="http://www.w3.org/2000/svg" height="30" viewBox="0 -960 960 960" width="30"><path d="m424-312 282-282-56-56-226 226-114-114-56 56 170 170ZM200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h560q33 0 56.5 23.5T840-760v560q0 33-23.5 56.5T760-120H200Zm0-80h560v-560H200v560Zm0-560v560-560Z"/></svg></div> :
+          <div className="svg-2"><svg xmlns="http://www.w3.org/2000/svg" height="28" viewBox="0 -960 960 960" width="28"><path d="M200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h560q33 0 56.5 23.5T840-760v560q0 33-23.5 56.5T760-120H200Zm0-80h560v-560H200v560Z"/></svg></div>
            }
         </button>
         <button
